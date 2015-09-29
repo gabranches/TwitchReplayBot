6e0be000737f9a6b12ae5d9ff33225ec1e38dae2
Gustavo Abranches <gabranches@gmail.com>
2015-09-28 22:40:34 -0400

    repo cleanup

--- app/config.py
+++
@@ -1,3 +0,0 @@
-CLIENT_ID = 'q63syowdemztjkcata7ccftboi1a45r'
-CLIENT_SECRET = '5x0eohaj8umrvgtaxhs0s7uv1r31x7'
-REDIRECT_URI = 'http://127.0.0.1:5000/callback'
--- app/tests/twitch_tests.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('/home/gabranches/twitch/app')
-
-import twitch
-
-
-print(twitch.get_replay('#zilea', 90))
