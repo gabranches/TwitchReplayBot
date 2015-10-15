140b25b9a2f4262c12f8636fba9789dd2f933601
Gustavo Abranches <gabranches@gmail.com>
2015-10-14 21:59:00 -0400

    fixed streams

--- twitch/streams.py
+++ twitch/streams.py
@@ -1,4 +1,3 @@
 CHANNELS = ('nl_kripp,eloise_ailv,claydavis64,reynad27,trumpsc'
             'forsenlol,amazhs,eslcsgo,sodapoppin,summit1g,froggen'
-            'lirik,giantwaffle,kaceytron,claydavis64,pangaeapanga,'
-            'trihex')
+            'lirik,giantwaffle,kaceytron,pangaeapanga,','trihex')
